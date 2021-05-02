3f7660fd67ec17da61438a251101c48362352652
miguel240 <measix.miguelr@gmail.com>
2021-05-02 20:34:51 +0200

    try 1 bonos

--- example/src/Foo/fixedLeg.cpp
+++ example/src/Foo/fixedLeg.cpp
@@ -1,4 +1,14 @@
-//
-// Created by miguelo on 24/4/21.
-//
+#include "fixedLeg.h"
+
+fixedLeg::FixedLeg(float interes) : interes_{interes} {};
+
+
+double fixedLeg::getPrice() {
+    double price;
+
+    for (auto fechaPago : calendarioPagos_) {
+        price += exp(- interes * getFraccion(today, fechaPagoo, '360')); // e^-rt
+    }
+}
+
 
--- example/src/Foo/fixedLeg.h
+++ example/src/Foo/fixedLeg.h
@@ -1,9 +1,22 @@
-#ifndef CFIXEDLEG_H
+#ifndef FIXEDLEG_H
 #define FIXEDLEG_H
 
-class FixedLeg {
+#include "boost/date_time/gregorian/gregorian.hpp"
+
+
+class FixedLeg : public Leg {
 public:
+    FixedLeg(float interes);
+
+    double getPrice();
+
 private:
+    std::vector<boost::gregorian::date> calendarioPagos_{boost::gregorian::date(2016, 10, 03),
+                                                         boost::gregorian::date(2016, 04, 03),
+                                                         boost::gregorian::date(2017, 10, 02),
+                                                         boost::gregorian::date(2017, 04, 02)};
+    float interes_;
+    auto today{boost::gregorian::date(2016, 04, 01)};
 };
 
 #endif
